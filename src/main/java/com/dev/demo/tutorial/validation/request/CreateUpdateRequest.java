a001c66729cf549a50b0ef7efa1f3e078782fb85
aliabbas-2012 <itsgeniusstar@gmail.com>
2025-01-22 16:23:39 +0500

    Naming conventions

--- src/main/java/com/dev/demo/tutorial/validation/request/CreateUpdateRequest.java
+++ src/main/java/com/dev/demo/tutorial/validation/request/CreateUpdateRequest.java
@@ -14,7 +14,7 @@ public class CreateUpdateRequest {
 
     @NotBlank(message = "Name is required.")
     @Size(min = 10, max = 50, message = "The name must be from 3 to 20 characters.")
-    @Unique(service = "TutorialService", fieldName = "email", message = "Title must be unique")
+    @Unique(service = "TutorialService", fieldName = "title", message = "Title must be unique")
     protected String title;
 
     protected String description;
