dfff7e3fba3383fa144aa5b2ae68cf8cc45da1c0
aliabbas-2012 <itsgeniusstar@gmail.com>
2025-02-03 15:20:15 +0500

    Add the tutorials filters

--- src/main/java/com/dev/demo/tutorial/repository/TutorialRepository.java
+++ src/main/java/com/dev/demo/tutorial/repository/TutorialRepository.java
@@ -5,11 +5,12 @@ import com.dev.demo.tutorial.model.Tutorial;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 
-public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> , JpaSpecificationExecutor<Tutorial> {
     boolean existsByTitle(String title);
 
     Optional<Tutorial> findByTitle(String title);
